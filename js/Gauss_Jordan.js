a7d0dd4a0bcd1fdae5186476a933d963d2348f6e
Serhii Vlokh <svlokh@c1r7p11.khpi.ucode-connect.study>
2021-04-29 18:24:40 +0300

    dfdfd

--- js/Gauss_Jordan.js
+++ js/Gauss_Jordan.js
@@ -16,10 +16,26 @@ function Gauss_Jordan(matrix, b) {
 
     console.log(I)
 
+    a11 = parseFloat(matrix[0][0])
 
-    for (let i = 0; i < matrix.length; i++) {
-        matrix[0][i] =
+    for (let j = 0; j < matrix.length; j++) {
+        matrix[0][j] = parseFloat(matrix[0][j]) / parseFloat(a11)
+        I[0][j] = parseFloat(I[0][j]) / a11
     }
+
+    // console.log("matrix", matrix)
+    // console.log("I", I)
+
+
+    for (let j = 1; j < matrix.length; j++) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[j][i] = matrix[j][i] - matrix[0][i] * matrix[j][i]
+        }
+        
+    }
+
+    console.log("matrix", matrix)
+    console.log("I", I)
 
 
     return x
