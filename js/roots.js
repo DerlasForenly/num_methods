d7598311c9638b228f9b116c00b2ee732534d7f1
Serhii Vlokh <svlokh@c1r7p11.khpi.ucode-connect.study>
2021-04-30 20:42:46 +0300

    roots

--- js/roots.js
+++ js/roots.js
@@ -1,10 +1,10 @@
 function check_roots(matrix, b, x) {
-    let current_x = [...x]
-    for (let i = 0; i < current_x.length; i++) {
-        current_x[i] = 0
+    for (let i = 0; i < matrix.length; i++) {
+        current_b = 0
+        for (let j = 0; j < matrix.length; j++)
+            current_b += matrix[i][j] * x[j]
+        if (Math.abs(current_b - b[i]) >= 1e-5)
+            return false
     }
-
-    
-
     return true
 }
